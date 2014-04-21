86dc68517202b57a66efa0ea62fcf62b34b6667d
Michel <michellang@gmail.com>
2014-04-21 02:17:17 +0200

    docs

--- man-roxygen/checker.r
+++ man-roxygen/checker.r
@@ -1,4 +1,4 @@
-#' @param x [\code{ANY}]\cr
+#' @param x [ANY]\cr
 #'  Object to check.
 #' @return Returns \code{TRUE} on success and an error message otherwise.
 #'  Intended to be used with \code{\link{test}}, \code{\link{assert}} or \code{\link{pass}}.
