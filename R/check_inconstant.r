1f0df82d0b813b155cb5fabd3dee3ae423e26d87
Michel <michellang@gmail.com>
2014-04-24 23:48:37 +0200

    cleanups

--- R/check_inconstant.r
+++ R/check_inconstant.r
@@ -3,13 +3,12 @@
 #' @template checker
 #' @inheritParams check_constant
 #' @export
-#' @useDynLib checkmate c_is_constant
 #' @family checker
 #' @examples
 #'  test(c(1, NA), "inconstant")
 #'  test(c(0, 1-0.9-0.1), "inconstant")
 #'  test(c(0, 1-0.9-0.1), "inconstant", tol = 0)
-check_inconstant= function(x, tol = .Machine$double.eps^0.5) {
+check_inconstant = function(x, tol = .Machine$double.eps^0.5) {
   if (check_constant_helper(x, tol))
     return("'%s' must have inconstant (varying) elements")
   return(TRUE)
