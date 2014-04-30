bcb7e3a3f19fd62ecbb56b6f2b8c0dc6e8579d2a
Michel <michellang@gmail.com>
2014-04-30 13:05:54 +0200

    docs

--- R/interface.r
+++ R/interface.r
@@ -8,18 +8,29 @@ getChecker = function(checker) {
   fun
 }
 
-#' Evaluate a check function and throw exception on failure
+#' Evaluate a check function
+#'
+#' All functions apply the check function \code{checker} on
+#' \code{x} but react different on the check result.
+#' Function \code{assert} invisibly returns \code{TRUE} on success and raises
+#' an R exeception otherwise.
+#' Function \code{check} returns the check result as single logical value
+#' Function \code{pass} returns \code{x} on success and raises an exception otherwise.
 #'
 #' @param x [ANY]\cr
 #'  Object to check.
-#' @param checker [character(1)]\cr
-#'  Name of the checker function without preceding \dQuote{check_}, e.g. \dQuote{numeric}.
+#' @param checker [\code{character(1)} or \code{function}]\cr
+#'  Either the name of a check function without preceding \dQuote{check_}
+#'  (e.g. \dQuote{numeric} for \code{\link{check_numeric}}) or a function.
+#'  The function is expected to return \code{TRUE} for a success check and
+#'  return a string with placeholder \dQuote{\%s} (see \code{\link[base]{sprintf}}
+#'  for \code{.var.name}.
 #' @param ... [ANY]\cr
 #'  Additional parameters passed down to \code{checker}.
 #' @param .var.name [\code{character(1)}]\cr
 #'  Phony name for \code{x} to use in error message.
 #'  If not provided, \code{x} will be substituted.
-#' @return \code{TRUE} on success, an exception is thrown otherwise.
+#' @return See Description.
 #' @family evaluate
 #' @export
 assert = function(x, checker, ..., .var.name) {
@@ -30,11 +41,7 @@ assert = function(x, checker, ..., .var.name) {
   invisible(TRUE)
 }
 
-#' Evaluate a check function to a logical value
-#'
-#' @inheritParams assert
-#' @return \code{TRUE} on success, \code{FALSE} otherwise.
-#' @family evaluate
+#' @rdname assert
 #' @export
 test = function(x, checker, ...) {
   checker = getChecker(checker)
@@ -42,11 +49,7 @@ test = function(x, checker, ...) {
 }
 
 
-#' Evaluate a check function and return object on success
-#'
-#' @inheritParams assert
-#' @return \code{x} on success, an exception is thrown otherwise.
-#' @family evaluate
+#' @rdname assert
 #' @export
 pass = function(x, checker, ..., .var.name) {
   checker = getChecker(checker)
