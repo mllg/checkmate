124bf4efe0d3e12d81fd21da5239c5df370b1b9f
Bernd Bischl <bernd_bischl@gmx.net>
2014-05-18 22:00:44 +0200

    clean up

--- tests/testthat/test_choice.R
+++ tests/testthat/test_choice.R
@@ -1,15 +1,15 @@
-context("check_elementOf")
+context("check_choice")
 
-test_that("check_elementOf", {
-  expect_error(test(character(0), "elementOf", letters), "length")
-  expect_error(test(NULL, "elementOf", letters), "atomic")
+test_that("check_choice", {
+  expect_error(test(character(0), "choice", letters), "length")
+  expect_error(test(NULL, "choice", letters), "atomic")
 
-  expect_true(test(1L, "elementOf", 1:10))
-  expect_false(test("ab", "elementOf", letters))
-  expect_false(test(NA_integer_, "elementOf", 1:10))
-  expect_error(test(1:2, "elementOf", 1:10), "length")
+  expect_true(test(1L, "choice", 1:10))
+  expect_false(test("ab", "choice", letters))
+  expect_false(test(NA_integer_, "choice", 1:10))
+  expect_error(test(1:2, "choice", 1:10), "length")
 
-  expect_true(assert(1L, "elementOf", 1:2))
-  expect_error(assert(-1, "elementOf", 1:2), "element of")
-  expect_error(assert(1L, "elementOf", list()), "atomic")
+  expect_true(assert(1L, "choice", 1:2))
+  expect_error(assert(-1, "choice", 1:2), "element of")
+  expect_error(assert(1L, "choice", list()), "atomic")
 })
