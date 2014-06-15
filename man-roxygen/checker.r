7504ff3bc31206dabd2615d1a5213592df894dd7
Bernd Bischl <bernd_bischl@gmx.net>
2014-06-15 22:14:49 +0200

    fix doc typo

--- man-roxygen/checker.r
+++ man-roxygen/checker.r
@@ -4,7 +4,7 @@
 #'  Name for \code{x}. Defaults to a heuristic to determine
 #'  the name using \code{\link[base]{deparse}} and \code{\link[base]{substitute}}.
 #' @return Depending on the function prefix:
-#'  If the check is successfull, all functions return \code{TRUE}.
-#'  If the check is not successfull, \code{assert<%= fn %>} throws an error message,
+#'  If the check is successful, all functions return \code{TRUE}.
+#'  If the check is not successful, \code{assert<%= fn %>} throws an error message,
 #'  \code{test<%= fn %>} returns \code{FALSE} and \code{check<%= fn %>} returns
 #'  a string with the error message.
