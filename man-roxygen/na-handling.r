0a4cb8cfe8b76c4448c4dde24b6b29e863055731
Michel <michellang@gmail.com>
2014-06-16 14:49:42 +0200

    NaN handling documented

--- man-roxygen/na-handling.r
+++ man-roxygen/na-handling.r
@@ -1,3 +1,3 @@
 #' @details This function does not distinguish between
 #' \code{NA}, \code{NA_integer_}, \code{NA_real_}, \code{NA_complex_}
-#' and \code{NA_character_}.
+#' \code{NA_character_} and \code{NaN}.
