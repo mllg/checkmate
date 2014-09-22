868de8c159faded3fe0ead29cedafb5fb5950e05
Michel <michellang@gmail.com>
2014-09-22 14:26:27 +0200

    added checkNames

---
+++ R/checkNames.r
@@ -0,0 +1,35 @@
+#' Check for names
+#'
+#' @templateVar fn Named
+#' @template checker
+#' @param type [character(1)]\cr
+#'  Select the check(s) to perform.
+#'  \dQuote{unnamed} checks \code{x} to be \code{NULL}.
+#'  \dQuote{named} (default) checks \code{x} for reguluar names which excludes names to be \code{NA} or emtpy (\code{""}).
+#'  \dQuote{unique} additionally tests for non-duplicated names.
+#'  \dQuote{strict} checks for unique names which comply to R's variable name restrictions.
+#' @useDynLib checkmate c_check_named
+#' @export
+#' @examples
+#' x = 1:3
+#' testNames(x, "unnamed")
+#' names(x) = letters[1:3]
+#' testNames(x, "unique")
+checkNames = function(x, type = "named") {
+  .Call("c_check_names", x, type, PACKAGE = "checkmate")
+}
+
+#' @rdname checkNames
+#' @useDynLib checkmate c_check_names
+#' @export
+assertNames = function(x, type = "named", .var.name) {
+  res = .Call("c_check_names", x, type, PACKAGE = "checkmate")
+  makeAssertion(res, vname(x, .var.name))
+}
+
+#' @rdname checkNames
+#' @useDynLib checkmate c_check_names
+#' @export
+testNames = function(x, type = "named") {
+  isTRUE(.Call("c_check_names", x, type, PACKAGE = "checkmate"))
+}
---
+++ tests/testthat/test_checkNames.r
@@ -0,0 +1,39 @@
+context("checkNames")
+
+test_that("checkNames", {
+  nn = letters[1:3]
+  expect_succ(Names, nn)
+  expect_fail(Names, nn, type = "unnamed")
+
+  expect_true(testNames(character(0)))
+  expect_false(testNames(NULL))
+  expect_false(testNames(integer(0)))
+
+  x = c("a", ".a")
+  expect_true(testNames(x))
+  expect_true(testNames(x, "unique"))
+  expect_true(testNames(x, "strict"))
+
+  expect_false(testNames(1))
+  expect_false(testNames(NA_character_))
+  expect_false(testNames(NA_integer_))
+  expect_false(testNames(""))
+
+  x = c("a", "a")
+  expect_true(testNames(x))
+  expect_false(testNames(x, "unique"))
+
+  expect_true(testNames("x", type = "strict"))
+  expect_true(testNames("..x", type = "strict"))
+  expect_true(testNames("x_1", type = "strict"))
+  expect_true(testNames("x.", type = "strict"))
+  expect_false(testNames("1", type = "strict"))
+  expect_false(testNames(".1", type = "strict"))
+  expect_false(testNames("..1", type = "strict"))
+  expect_false(testNames("x ", type = "strict"))
+  expect_false(testNames("ä", type = "strict"))
+  expect_error(assertNames(c("a", "a"), "unique"), "unique")
+
+  x = c("a", "1")
+  expect_error(assertNames(x, "strict"), "naming rules")
+})
