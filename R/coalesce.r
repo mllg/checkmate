92ad38534fb38afac835708ccb022410fb7aa5ab
Michel <michellang@gmail.com>
2015-07-05 21:39:01 +0200

    fixed coalesce docs

--- R/coalesce.r
+++ R/coalesce.r
@@ -4,6 +4,12 @@
 #' @description
 #' Returns the left hand side if not missing nor \code{NULL}, and
 #' the right hand side otherwise.
+#'
+#' @param lhs [any]\cr
+#' Left hand side of the operator. Is returned, if not missing or \code{NULL}.
+#' @param rhs [any]\cr
+#' Right hand side of the operator. Is returned, if \code{lhs} is missing or \code{NULL}.
+#' @return Either \code{lhs} or \code{rhs}.
 #' @export
 #' @examples
 #' print(NULL %??% 1 %??% 2)
