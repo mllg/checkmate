8474c802e24430c7955c298d1ecafa9b862a60b2
Michel <michellang@gmail.com>
2015-07-09 16:02:25 +0200

    added a test for scalar_na

--- tests/testthat/test_checkScalarNA.r
+++ tests/testthat/test_checkScalarNA.r
@@ -5,6 +5,7 @@ test_that("checkScalarNA", {
   expect_true(testScalarNA(NA_real_))
   expect_false(testScalarNA(1))
   expect_false(testScalarNA(rep(NA_character_, 2)))
+  expect_true(is.expectation(expect_scalar_na(NA)))
 
   expect_error(assertScalarNA(integer(0)), "missing value")
 })
