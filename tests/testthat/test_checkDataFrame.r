02eece1659555748ce7d294f7d1fe8e92f59b2d0
Michel <michellang@gmail.com>
2015-12-08 11:36:16 +0100

    added tests for magrittr

--- tests/testthat/test_checkDataFrame.r
+++ tests/testthat/test_checkDataFrame.r
@@ -82,21 +82,3 @@ test_that("missing values are detected", {
   expect_false(testDataFrame(x, any.missing = FALSE))
   expect_false(testDataFrame(x, all.missing = FALSE))
 })
-
-test_that("data.table is supported", {
-  skip_if_not_installed("data.table")
-  library(data.table)
-  myobj = as.data.table(iris)
-  expect_succ_all(DataFrame, myobj)
-  expect_true(testDataFrame(myobj, nrow = 150, min.cols = 2, any.missing = FALSE, col.names = "strict"))
-  expect_true(testDataFrame(data.table()))
-})
-
-test_that("dplyr::data_frame is supported", {
-  skip_if_not_installed("dplyr")
-  library(dplyr)
-  myobj = as_data_frame(iris)
-  expect_succ_all(DataFrame, myobj)
-  expect_true(testDataFrame(myobj, nrow = 150, min.cols = 2, any.missing = FALSE, col.names = "strict"))
-  expect_true(testDataFrame(data_frame()))
-})
