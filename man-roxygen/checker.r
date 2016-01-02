5bebe507f3ccbbb0cb4f7eb7960a2f22f74c4601
Michel <michellang@gmail.com>
2016-01-02 02:03:03 +0100

    updated docs

--- man-roxygen/checker.r
+++ man-roxygen/checker.r
@@ -4,10 +4,10 @@
 #'  Name for \code{x}. Defaults to a heuristic to determine
 #'  the name using \code{\link[base]{deparse}} and \code{\link[base]{substitute}}.
 #' @return Depending on the function prefix:
-#'  If the check is successful, all functions return \code{TRUE}. If the check
+#'  If the check is successful, the functions return \code{TRUE}. If the check
 #'  is not successful, \code{assert<%= fn %>}/\code{assert_<%= convertCamelCase(fn) %>}
 #'  throws an error message, \code{test<%= fn %>}/\code{test_<%= convertCamelCase(fn) %>}
 #'  returns \code{FALSE},
 #'  and \code{check<%= fn %>} returns a string with the error message.
-#'  The function \code{expect_<%= convertCamelCase(fn) %>} will return an
+#'  The function \code{expect_<%= convertCamelCase(fn) %>} always returns an
 #'  \code{\link[testthat]{expectation}}.
