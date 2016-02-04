b7186fa835c1a0bbeb078479908a25396cde8bb3
Michel <michellang@gmail.com>
2016-02-04 15:33:05 +0100

    improved coverage

--- tests/testthat/test_makeFunction.r
+++ tests/testthat/test_makeFunction.r
@@ -5,6 +5,11 @@ test_that("makeAssertion", {
   y = makeAssertionFunction(checkFlag, c.fun = "c_check_flag")
   expect_identical(formals(x), formals(y))
   expect_equal(body(x), body(y))
+
+  x = assertList
+  y = makeAssertionFunction(checkList)
+  expect_identical(formals(x), formals(y))
+  expect_equal(body(x), body(y))
 })
 
 test_that("makeTest", {
@@ -12,11 +17,21 @@ test_that("makeTest", {
   y = makeTestFunction(checkFlag, c.fun = "c_check_flag")
   expect_identical(formals(x), formals(y))
   expect_equal(body(x), body(y))
+
+  x = testList
+  y = makeTestFunction(checkList)
+  expect_identical(formals(x), formals(y))
+  expect_equal(body(x), body(y))
 })
 
 test_that("makeExpectation", {
   x = expect_flag
   y = makeExpectationFunction(checkFlag, c.fun = "c_check_flag")
+  expect_identical(formals(x), formals(y))
+  expect_equal(body(x), body(y))
+
+  x = expect_list
+  y = makeExpectationFunction(checkList)
   expect_identical(formals(x), formals(y))
   expect_equal(body(x), body(y))
 })
