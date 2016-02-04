f93611ab3cade254d1bc2e2ed56d996fb6a117c0
Michel <michellang@gmail.com>
2016-02-04 12:54:15 +0100

    added test for makeXFunctions

---
+++ tests/testthat/test_makeFunction.r
@@ -0,0 +1,22 @@
+context("makeXFunction")
+
+test_that("makeAssertion", {
+  x = assertFlag
+  y = makeAssertionFunction(checkFlag, c.fun = "c_check_flag")
+  expect_identical(formals(x), formals(y))
+  expect_equal(body(x), body(y))
+})
+
+test_that("makeTest", {
+  x = testFlag
+  y = makeTestFunction(checkFlag, c.fun = "c_check_flag")
+  expect_identical(formals(x), formals(y))
+  expect_equal(body(x), body(y))
+})
+
+test_that("makeExpectation", {
+  x = expect_flag
+  y = makeExpectationFunction(checkFlag, c.fun = "c_check_flag")
+  expect_identical(formals(x), formals(y))
+  expect_equal(body(x), body(y))
+})
