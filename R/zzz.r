5e53f8832ce2e2700d0677d208dcb7af564790d1
Michel <michellang@gmail.com>
2016-02-12 09:18:49 +0100

    use backports package

--- R/zzz.r
+++ R/zzz.r
@@ -3,6 +3,7 @@
 #' \describe{
 #'   \item{Homepage:}{\url{https://github.com/mllg/checkmate}}
 #' }
+#' @importFrom backports dir.exists
 #' @importFrom utils head
 #' @importFrom utils tail
 "_PACKAGE"
