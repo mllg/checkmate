5b86a107627c1ba8c0c7b406de53e61c8c3f6cbd
Michel <michellang@gmail.com>
2016-02-14 19:41:03 +0100

    added a test

--- tests/testthat/test_checkString.r
+++ tests/testthat/test_checkString.r
@@ -5,6 +5,7 @@ test_that("checkString", {
   expect_succ_all(String, "")
   expect_fail_all(String, "", empty.ok = FALSE)
   expect_fail_all(String, 1L)
+  expect_fail_all(String, NA_character_, na.ok = TRUE, empty.ok = FALSE)
 
   expect_false(testString(character(0)))
   expect_false(testString(NULL))
