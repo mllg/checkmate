8cebe04bbf9a6a7cb8593e45514fe06e4cfcf237
Michel <michellang@gmail.com>
2016-02-14 19:42:25 +0100

    removed dependency on backports package

--- R/zzz.r
+++ R/zzz.r
@@ -3,7 +3,6 @@
 #' \describe{
 #'   \item{Homepage:}{\url{https://github.com/mllg/checkmate}}
 #' }
-#' @importFrom backports dir.exists
 #' @importFrom utils head
 #' @importFrom utils tail
 "_PACKAGE"
