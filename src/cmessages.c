854c85e82ed74d91fbd730b948dabebf8109d4c3
Michel <michellang@gmail.com>
2016-04-01 00:05:25 +0200

    moved package to test checkmate to own repo

--- src/cmessages.c
+++ src/cmessages.c
@@ -11,21 +11,3 @@ msg_t make_msg(const char *fmt, ...) {
     va_end(vargs);
     return msg;
 }
-
-const char * guessType(SEXP x) {
-    SEXP attr = getAttrib(x, R_ClassSymbol);
-    if (!isNull(attr))
-        return CHAR(STRING_ELT(attr, 0));
-
-    attr = getAttrib(x, R_DimSymbol);
-    if (!isNull(attr) && isVectorAtomic(x))
-        return length(attr) == 2 ? "matrix" : "array";
-
-    return type2char(TYPEOF(x));
-}
-
-SEXP make_type_error(SEXP x, const char *expected) {
-    char msg[CMSGLEN];
-    snprintf(msg, CMSGLEN, "Must be of type '%s', not '%s'", expected, guessType(x));
-    return ScalarString(mkChar(msg));
-}
--- src/helper.c
+++ src/helper.c
@@ -2,6 +2,18 @@
 #include "any_missing.h"
 #include "is_integerish.h"
 
+const char * guessType(SEXP x) {
+    SEXP attr = getAttrib(x, R_ClassSymbol);
+    if (!isNull(attr))
+        return CHAR(STRING_ELT(attr, 0));
+
+    attr = getAttrib(x, R_DimSymbol);
+    if (!isNull(attr) && isVectorAtomic(x))
+        return length(attr) == 2 ? "matrix" : "array";
+
+    return type2char(TYPEOF(x));
+}
+
 Rboolean isStrictlyNumeric(SEXP x) {
     switch(TYPEOF(x)) {
         case REALSXP: return TRUE;
