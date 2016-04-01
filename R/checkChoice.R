f4c345f8b1a83f5b0f94e1b4022c5f25e1ee0301
Michel <michellang@gmail.com>
2016-04-02 01:24:42 +0200

    remove null.ok from set functions (would be an interface change)

--- R/checkChoice.R
+++ R/checkChoice.R
@@ -5,15 +5,12 @@
 #' @param choices [\code{atomic}]\cr
 #'  Set of possible values.
 #' @template checker
-#' @template null.ok
 #' @family set
 #' @export
 #' @examples
 #' testChoice("x", letters)
-checkChoice = function(x, choices, null.ok = FALSE) {
+checkChoice = function(x, choices) {
   qassert(choices, "a")
-  if (identical(null.ok, TRUE) && is.null(x))
-    return(TRUE)
   if (!qtest(x, "a1") || x %nin% choices)
     return(sprintf("Must be element of set {'%s'}", collapse(unique(choices), "','")))
   return(TRUE)
