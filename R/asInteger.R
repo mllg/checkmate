d638a7cfaf9cb3c293b21c1b500da9cbf9b71f2e
Michel <michellang@gmail.com>
2016-04-18 11:34:51 +0200

    added proposal for matchArg

--- R/asInteger.R
+++ R/asInteger.R
@@ -13,8 +13,7 @@
 #' @param ... [any]\cr
 #'  Additional arguments passed to \code{\link{assertIntegerish}}.
 #' @template tol
-#' @param add [\code{AssertCollection}]\cr
-#'  Collection to store assertions. See \code{\link{AssertCollection}}.
+#' @template add
 #' @template var.name
 #' @return Converted \code{x}.
 #' @export
@@ -22,7 +21,7 @@
 #' asInteger(c(1, 2, 3))
 #' asCount(1)
 #' asInt(1)
-asInteger = function(x, ..., tol = sqrt(.Machine$double.eps), add = NULL, .var.name = vname(x)) {
+asInteger = function(x, ..., tol = sqrt(.Machine$double.eps), .var.name = vname(x), add = NULL) {
   assertIntegerish(x, ..., tol = tol, .var.name = .var.name, add = add)
   storage.mode(x) = "integer"
   x
@@ -35,7 +34,7 @@ asInteger = function(x, ..., tol = sqrt(.Machine$double.eps), add = NULL, .var.n
 #' @param na.ok [\code{logical(1)}]\cr
 #'  Are missing values allowed? Default is \code{FALSE}.
 #' @export
-asCount = function(x, na.ok = FALSE, positive = FALSE, tol = sqrt(.Machine$double.eps), add = NULL, .var.name = vname(x)) {
+asCount = function(x, na.ok = FALSE, positive = FALSE, tol = sqrt(.Machine$double.eps), .var.name = vname(x), add = NULL) {
   assertCount(x, na.ok, positive, tol, .var.name = .var.name, add = add)
   storage.mode(x) = "integer"
   x
@@ -44,7 +43,7 @@ asCount = function(x, na.ok = FALSE, positive = FALSE, tol = sqrt(.Machine$doubl
 #' @rdname asInteger
 #' @template bounds
 #' @export
-asInt = function(x, na.ok = FALSE, lower = -Inf, upper = Inf, tol = sqrt(.Machine$double.eps), add = NULL, .var.name = vname(x)) {
+asInt = function(x, na.ok = FALSE, lower = -Inf, upper = Inf, tol = sqrt(.Machine$double.eps), .var.name = vname(x), add = NULL) {
   assertInt(x, na.ok, lower, upper, tol, .var.name = .var.name, add = add)
   storage.mode(x) = "integer"
   x
---
+++ R/matchArg.R
@@ -0,0 +1,42 @@
+#' Partial Argument Matching
+#'
+#' @description
+#' This is an extensions to \code{\link[base]{match.arg}} with support for \code{\link{AssertCollection}}.
+#' The behaviour is very similar to \code{\link[base]{match.arg}}, except that \code{NULL} is not
+#' a valid value for \code{x}.
+#'
+#' @param x [character]\cr
+#'  User provided argument to match.
+#' @param choices [character()]\cr
+#'  Candidates to match \code{x} with.
+#' @param several.ok [logical(1)]\cr
+#'  If \code{TRUE}, multiple matches are allowed, cf. \code{\link[base]{match.arg}}.
+#' @template add
+#' @template var.name
+#' @return Subset of \code{choices}.
+#' @export
+#' @examples
+#' matchArg("k", choices = c("kendall", "pearson"))
+matchArg = function(x, choices, several.ok = FALSE, .var.name = vname(x), add = NULL) {
+  assertCharacter(choices, min.len = 1L)
+  assertFlag(several.ok)
+
+  if (several.ok) {
+    if (identical(x, choices))
+      return(x)
+    assertCharacter(x, min.len = 1L, .var.name = .var.name, add = add)
+    x = choices[pmatch(x, choices, nomatch = 0L, duplicates.ok = TRUE)]
+    assertSubset(x, choices, empty.ok = FALSE, .var.name = .var.name, add = add)
+  } else {
+    if (identical(x, choices))
+      return(x[1L])
+    assertCharacter(x, len = 1L, .var.name = .var.name, add = add)
+    x = choices[pmatch(x, choices, nomatch = 0L, duplicates.ok = FALSE)]
+    assertChoice(x, choices, .var.name = .var.name, add = add)
+  }
+  x
+}
+
+#' @rdname matchArg
+#' @export
+match_arg = matchArg
