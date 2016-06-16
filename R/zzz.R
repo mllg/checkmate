c83ee376b20dc0f5b318e7b7be08132593e2d52d
Michel Lang <michellang@gmail.com>
2016-06-16 14:27:55 +0200

    speedup checkListTypes

--- R/zzz.R
+++ R/zzz.R
@@ -11,3 +11,26 @@
 .onUnload <- function (libpath) {
   library.dynam.unload("checkmate", libpath) # nocov
 }
+
+### Globals
+.CHECKLISTTYPEFUNS = list(
+  "logical" = is.logical,
+  "integer" = is.integer,
+  "integerish" = function(x) isIntegerish(x),
+  "double" = is.double,
+  "numeric" = is.numeric,
+  "complex" = is.complex,
+  "character" = is.character,
+  "factor" = is.factor,
+  "atomic" = is.atomic,
+  "vector" = is.vector,
+  "atomicvector" = function(x) !is.null(x) && is.atomic(x),
+  "array" = is.array,
+  "matrix" = is.matrix,
+  "function" = is.function,
+  "environment" = is.environment,
+  "list" = is.list,
+  "null" = is.null
+)
+
+.OS = c("windows", "mac", "linux", "solaris")[match(tolower(Sys.info()["sysname"]), c("windows", "darwin", "linux", "sunos"))]
