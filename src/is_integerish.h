0fb85dcecb353daf6cb71cb135dfe2077dab99b9
Michel Lang <michellang@gmail.com>
2016-07-06 01:42:03 +0200

    cleanup/rename

--- src/is_integerish.h
+++ src/is_integerish.h
@@ -1,11 +1,11 @@
 #ifndef CHECKMATE_IS_INTEGERISH_H_
 #define CHECKMATE_IS_INTEGERISH_H_
 
+#define USE_RINTERNALS
 #include <R.h>
 #include <Rinternals.h>
-#define USE_RINTERNALS
-#define INTEGERISH_DEFAULT_TOL sqrt(DOUBLE_EPS)
 
+#define INTEGERISH_DEFAULT_TOL sqrt(DOUBLE_EPS)
 Rboolean isIntegerish(SEXP, double);
 SEXP c_is_integerish(SEXP, SEXP);
 
