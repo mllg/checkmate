9b83e2523741e564b7ec30542f9089cf988ca011
Michel Lang <michellang@gmail.com>
2016-07-08 16:12:30 +0200

    use conditions package in C code

--- inst/include/checkmate.h
+++ inst/include/checkmate.h
@@ -4,28 +4,18 @@
 #include <R.h>
 #include <Rinternals.h>
 #include <Rdefines.h>
+#include <R_ext/Rdynload.h>
 #include <R_ext/Visibility.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-Rboolean attribute_hidden qtest(SEXP x, const char *rule) {
-  static Rboolean(*fun)(SEXP, const char *) = NULL;
-  if (fun == NULL)
-    fun = (Rboolean(*)(SEXP, const char *)) R_GetCCallable("checkmate", "qtest");
-  return fun(x, rule);
-}
-
-SEXP attribute_hidden qassert(SEXP x, const char *rule, const char *name) {
-  static SEXP(*fun)(SEXP, const char *, const char *) = NULL;
-  if (fun == NULL)
-    fun = (SEXP(*)(SEXP, const char *, const char *)) R_GetCCallable("checkmate", "qassert");
-  return fun(x, rule, name);
-}
-
-#endif
+Rboolean attribute_hidden qtest(SEXP x, const char *rule);
+SEXP attribute_hidden qassert(SEXP x, const char *rule, const char *name);
 
 #ifdef __cplusplus
 }
+#endif
+
 #endif
