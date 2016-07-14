86fe9fb36c8529c6e5bf62c2812733823b087430
Dénes Tóth <tdeenes@users.noreply.github.com>
2016-07-14 19:41:46 +0200

    Fix doc (return value of assert* functions) (#78)
    - assert* functions do not return TRUE but the input object
    - template (checker.R) updated and Rd files re-created

--- man-roxygen/checker.R
+++ man-roxygen/checker.R
@@ -1,7 +1,14 @@
 #' @return Depending on the function prefix:
-#'  If the check is successful, the functions return \code{TRUE}. If the check
-#'  is not successful, \code{assert<%= fn %>}/\code{assert_<%= convertCamelCase(fn) %>}
-#'  throws an error message, \code{test<%= fn %>}/\code{test_<%= convertCamelCase(fn) %>}
+#'  If the check is successful, the functions 
+#'  \code{assert<%= fn %>}/\code{assert_<%= convertCamelCase(fn) %>} return 
+#'  \code{x} invisibly, whereas
+#'  \code{check<%= fn %>}/\code{check_<%= convertCamelCase(fn) %>} and 
+#'  \code{test<%= fn %>}/\code{test_<%= convertCamelCase(fn) %>} return 
+#'  \code{TRUE}.
+#'  If the check is not successful, 
+#'  \code{assert<%= fn %>}/\code{assert_<%= convertCamelCase(fn) %>}
+#'  throws an error message, 
+#'  \code{test<%= fn %>}/\code{test_<%= convertCamelCase(fn) %>}
 #'  returns \code{FALSE},
 #'  and \code{check<%= fn %>} returns a string with the error message.
 #'  The function \code{expect_<%= convertCamelCase(fn) %>} always returns an
