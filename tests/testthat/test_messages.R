f8da869b65bfc69f2890645ce01496422d7d0696
Michel Lang <michellang@gmail.com>
2016-08-22 10:35:08 +0200

    improved test

--- tests/testthat/test_messages.R
+++ tests/testthat/test_messages.R
@@ -3,5 +3,5 @@ context("generated messages")
 test_that("No extra strings attached to generated error messages", {
   foo = function(XX) assertFlag(XX)
   expect_error(foo(iris), "^Assertion on 'XX'")
-  expect_error(foo(iris), "not 'data.frame'.$")
+  expect_error(foo(iris), "not 'data.frame'\\.$")
 })
