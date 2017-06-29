9efb3e6411470b9e13c0abd8ffd66544e2b957d6
Will Beasley <wibeasley@hotmail.com>
2017-06-29 07:26:18 -0500

    Missing dates not compared to lower & upper bounds (#110)
    Merged, thanks!

--- tests/testthat/test_checkDate.R
+++ tests/testthat/test_checkDate.R
@@ -30,3 +30,49 @@ test_that("checkDate", {
 
   expect_error(assertDate(letters, unique = TRUE), "character")
 })
+
+test_that("NAs are ignored for dates' lower-bound", {
+  # Define and test a nomal date vector, and an empty date vector.
+  d <- as.Date(c("2015-01-01", "2016-01-01", NA_character_, "2017-01-01"))
+  empty <- as.Date(character(0))
+  nas <- as.Date(NA_character_, NA_character_, NA_character_)
+
+  # Bounds pass/fail appropriately when missing values are legal.
+  expect_true( testDate(d    , lower = "1980-01-01", any.missing = TRUE ))
+  expect_false(testDate(d    , lower = "2016-01-01", any.missing = TRUE ))
+
+  # Bounds are ignored when missing values are illegal (and the vector contains missing values).
+  expect_false(testDate(d    , lower = "1980-01-01", any.missing = FALSE))
+  expect_false(testDate(d    , lower = "2016-01-01", any.missing = FALSE))
+
+  # Zero-length date vectors never fail with a lower bound.
+  expect_true( testDate(empty, lower  ="2030-01-01", any.missing = TRUE ))
+  expect_true( testDate(empty, lower  ="2030-01-01", any.missing = FALSE))
+
+  # NA date vectors
+  expect_true( testDate(nas  , lower  ="2030-01-01", any.missing = TRUE ))
+  expect_false(testDate(nas  , lower  ="2030-01-01", any.missing = FALSE))
+})
+
+test_that("NAs are ignored for dates' upper-bound", {
+  # Define and test a nomal date vector, and an empty date vector.
+  d <- as.Date(c("2015-01-01", "2016-01-01", NA_character_, "2017-01-01"))
+  empty <- as.Date(character(0))
+  nas <- as.Date(NA_character_, NA_character_, NA_character_)
+
+  # Bounds pass/fail appropriately when missing values are legal.
+  expect_true( testDate(d    , upper = "2020-01-01", any.missing = TRUE ))
+  expect_false(testDate(d    , upper = "2016-01-01", any.missing = TRUE ))
+
+  # Bounds are ignored when missing values are illegal (and the vector contains missing values).
+  expect_false(testDate(d    , upper = "2020-01-01", any.missing = FALSE))
+  expect_false(testDate(d    , upper = "2016-01-01", any.missing = FALSE))
+
+  # Zero-length date vectors never fail with a upper bound.
+  expect_true( testDate(empty, upper = "2000-01-01", any.missing = FALSE))
+  expect_true( testDate(empty, upper = "2000-01-01", any.missing = FALSE))
+
+  # NA date vectors
+  expect_true( testDate(nas  , lower  ="2030-01-01", any.missing = TRUE ))
+  expect_false(testDate(nas  , lower  ="2030-01-01", any.missing = FALSE))
+})
