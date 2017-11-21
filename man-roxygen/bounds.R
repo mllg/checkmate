56bb6e3bf69c5fe64bd05542fe8b1c336698153a
Michel Lang <michellang@gmail.com>
2017-11-21 09:53:31 +0100

    update docs (#45)

--- man-roxygen/bounds.R
+++ man-roxygen/bounds.R
@@ -1,4 +1,4 @@
 #' @param lower [\code{numeric(1)}]\cr
-#'  Lower value all elements of \code{x} must be greater than.
+#'  Lower value all elements of \code{x} must be greater than or equal to.
 #' @param upper [\code{numeric(1)}]\cr
-#'  Upper value all elements of \code{x} must be lower than.
+#'  Upper value all elements of \code{x} must be lower than or equal to.
