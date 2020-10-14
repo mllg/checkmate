8cee2cb7d1c50d6f51c76fc465e39bdddf4e9482
Michel Lang <michellang@gmail.com>
2020-10-14 11:20:51 +0200

    update docs

--- man-roxygen/typed.missing.R
+++ man-roxygen/typed.missing.R
@@ -1,4 +1,5 @@
 #' @param typed.missing [\code{logical(1)}]\cr
 #'  If set to \code{FALSE} (default), all types of missing values (\code{NA}, \code{NA_integer_},
-#'  \code{NA_real_}, \code{NA_character_} and \code{NA_character_}) are allowed during type checking.
+#'  \code{NA_real_}, \code{NA_character_} or \code{NA_character_}) as well as empty vectors are allowed
+#'  while type-checking atomic input.
 #'  Set to \code{FALSE} to enable strict type checking.
