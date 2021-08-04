c6e6a8fb8acd5b3f0eb07473c19c0739297442f4
Petter Hopp <PetterHopp@users.noreply.github.com>
2021-08-04 18:40:23 +0200

    doc: fix documentation for checkNames (#209)
    * doc: fix documentation for checkNames
    
    Changed templateVar to Names.
    Changed description of param disjunct.from.
    
    * doc: update checker.R
    
    Suggest to standardize decription in documentation so both function names in snake_case and camelCase is mentioned

--- man-roxygen/checker.R
+++ man-roxygen/checker.R
@@ -10,6 +10,7 @@
 #'  throws an error message, 
 #'  \code{test<%= fn %>}/\code{test_<%= convertCamelCase(fn) %>}
 #'  returns \code{FALSE},
-#'  and \code{check<%= fn %>} returns a string with the error message.
+#'  and \code{check<%= fn %>}/\code{check_<%= convertCamelCase(fn) %>} 
+#'  return a string with the error message.
 #'  The function \code{expect_<%= convertCamelCase(fn) %>} always returns an
 #'  \code{\link[testthat]{expectation}}.
