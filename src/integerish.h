f9e0ba01d59f2827fa4264789db24c28bb26ca98
Michel Lang <michellang@gmail.com>
2022-03-27 16:23:07 +0200

    remove deprecated macro DOUBLE_EPS

--- src/integerish.h
+++ src/integerish.h
@@ -4,8 +4,9 @@
 #include <R.h>
 #include <Rinternals.h>
 #include <R_ext/Visibility.h>
+#include <float.h>
 
-#define INTEGERISH_DEFAULT_TOL sqrt(DOUBLE_EPS)
+#define INTEGERISH_DEFAULT_TOL sqrt(DBL_EPSILON)
 typedef enum { INT_OK, INT_TYPE, INT_RANGE, INT_TOL, INT_COMPLEX } cm_int_check_t;
 typedef struct { R_xlen_t pos; cm_int_check_t err; } cm_int_err_t;
 
