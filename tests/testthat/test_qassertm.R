e7b8747bb988ce15d0e7c0343c2d76410e2b4319
WillForan <willforan@gmail.com>
2022-07-23 10:54:51 -0400

    qassert_all: one rule, many vars. actually address #115

--- tests/testthat/test_qassertm.R
+++ tests/testthat/test_qassertm.R
@@ -1,10 +1,11 @@
 context("qassertm")
 
 xb = logical(10)
+xb2 = logical(10)
 xi = integer(10)
 xr = double(10)
 
-test_that("fail on any, report first", {
+test_that("qassertm: fail on any, report first", {
   # none
   expect_error(qassertm(), NA)
   # one
@@ -22,4 +23,20 @@ test_that("fail on any, report first", {
   # two wrong. only reports first
   expect_error(qassertm(xb="b", xi="l", xr="b"),
                regexp = "'xi' .*'list', not 'integer'")
+})
+
+test_that("qassert_all", {
+  # global
+  expect_error(qassert_all('b',xb,xb2), NA)
+  expect_error(qassert_all('n',xb,xb2),
+               regexp="'xb' .*numeric', not 'logical'")
+  expect_error(qassert_all('r',xr,xb),
+               regexp="'xb' .*double', not 'logical'")
+
+  # within function. reusing gobal names to test scope
+  expect_error((function(xb,xb2) qassert_all("n"))(1,2), NA)
+  expect_error((function(xb,xb2) qassert_all("n"))(1,TRUE),
+               regexp="'xb2' .*numeric', not 'logical'")
+  expect_error((function(xb,xb2) qassert_all("n"))(1),
+               regexp='"xb2" is missing')
 })
